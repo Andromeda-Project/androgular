f1d090976aea97f6eb139386a776f19763809e7e
Donald Organ <donald.organ@disney.com>
2015-02-14 14:39:56 -0500

    Setting up grunt

---
+++ grunt/watch.js
@@ -0,0 +1,11 @@
+module.exports = {
+    all: {
+        files: [
+            '<%= jshint.all %>',
+            'grunt/*.js'
+        ],
+        tasks: [
+            'default'
+        ]
+    }
+};
